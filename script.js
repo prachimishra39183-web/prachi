cf741ddafc7fb3eb838167c668d86dd503336f1c
prachimishra39183-web <prachimishra39183@gmail.com>
2026-01-18 20:56:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -124,4 +124,11 @@ const buttons = document.querySelectorAll('.view-events-btn');
     });
   });
 
-  
+  var modal1 = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
